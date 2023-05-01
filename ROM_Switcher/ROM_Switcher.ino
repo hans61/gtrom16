8a8f95f08a7522284718193be61aa80a864226e7
hans61 <80166258+hans61@users.noreply.github.com>
2023-05-01 21:11:01 +0200

    first version, tested, needs improvement

--- ROM_Switcher/ROM_Switcher.ino
+++ ROM_Switcher/ROM_Switcher.ino
@@ -39,40 +39,56 @@ byte display[] = {
 
 byte tmp;
 byte romIdx;               // ROM Index (selected ROM)
+byte romLastIdx = 17;
+byte eepromAddr;           // ROM Index (selected ROM)
 long buttonTime;           // Keystroke duration
 long lastAction;           // Start button pressed
 byte lastButtonState = 0;  // Status of the last keystroke
+byte saveEeprom = 1;
 
 // Key query
 // return value
-// 1 = Key pressed shorter 50 ms
-// 2 = Key was pressed
-// 3 = Key was pressed for a long time (>2 seconds)
-// 4 = Key was pressed for a very long time (>5 seconds)
+// 2 = Key pressed shorter 50 ms
+// 3 = Key was pressed
+// 4 = Key was pressed for a long time (>1 seconds)
+// 5 = Key was pressed for a very long time (>3 seconds)
+// 6 = Key released for a long time (>2 seconds)
 byte getButton() {
-  if ((PINB & (1 << 3)) == 0) { // Key pressed
-    if (lastButtonState == 0) { // not pressed before
-      lastButtonState = 1;
-      lastAction = millis();
-    } else { // Key is still pressed
-      if ((millis() - lastAction) > 5000) lastButtonState = 5;
-    }
-  } else {                     // Key released
-    if (lastButtonState > 0) { // Key was pressed
-      lastButtonState = 0;
-      buttonTime = millis() - lastAction;
-      if (buttonTime > 5000) {
-        return 4;
-      } else if (buttonTime > 2000) {
-        return 3;
-      } else if (buttonTime > 50) {
-        return 2;
-      } else {
-        return 1;
+  if ((millis() - lastAction) > 50 ) {
+    if ((PINB & (1 << 3)) == 0) { // Key pressed
+      if (lastButtonState == 0) { // not pressed before
+        lastButtonState = 1;
+        lastAction = millis();
+      } else { // Key is still pressed
+        if ((millis() - lastAction) > 5000) lastButtonState = 5;
+      }
+    } else {                     // Key released
+      if (lastButtonState > 0) { // Key was pressed [Taste war gedrückt, wurde eben losgelassen]
+        lastButtonState = 0;
+        buttonTime = millis() - lastAction;
+        lastAction = millis();
+        if (buttonTime > 3000) {
+          return 5;
+        } else if (buttonTime > 1000) {
+          return 4;
+        } else if (buttonTime > 50) {
+          return 3;
+        } else {
+          //lastButtonState = 1;
+          return 2;
+        }
+      } else {                 // Key is not pressed and was not pressed
+        if ((millis() - lastAction) > 2000) lastButtonState = 6;
+        //        buttonTime = millis() - lastAction;
+        //        if (buttonTime > 3000) {
+        //          return 1;
+        //        }
       }
     }
+    return lastButtonState;
+  } else {                  // Debounce time
+    return 0;
   }
-  return lastButtonState;
 }
 
 // print char
@@ -90,28 +106,58 @@ void setAddress(byte id) {
   }
 }
 
+void writeEEPROM() {
+  if (eepromAddr < 2 || eepromAddr > 128) { // Address must be even and between 2..128
+    eepromAddr = 2;
+    EEPROM.update(ADDR, eepromAddr);
+  }
+  tmp = EEPROM.read(eepromAddr + 1);        // Increase write counter for cell
+  tmp--;
+  EEPROM.update(eepromAddr + 1, tmp);
+  if (tmp == 0) {                           // switch to the next cell on overflow
+    eepromAddr += 2;
+    if (eepromAddr > 128) eepromAddr = 2;
+    EEPROM.update(ADDR, eepromAddr);        // Set pointer to current cell
+  }
+  EEPROM.update(eepromAddr, romIdx);        // Save ROM index
+}
+
 void setup() {
   DDRA = 0xFF;
-  DDRB = 0x07; 	               // 0b00000111;
-  romIdx = EEPROM.read(ADDR);  // read default index from eeprom
+  DDRB = 0x07; 	                            // 0b00000111;
+  eepromAddr = EEPROM.read(ADDR) & 0b01111110;
+  romIdx = EEPROM.read(eepromAddr);         // read default index from eeprom
   romIdx &= 0x0F;
+  setAddress(romIdx);
 }
 
 void loop() {
-  setDisplay(romIdx & 0x0F);   // show selected bank
-  setAddress(romIdx);          // activate selected bank
+  if (romIdx != romLastIdx) {
+    setAddress(romIdx);          // activate selected bank
+    setDisplay(romIdx & 0x0F);   // show selected bank
+    romLastIdx = romIdx;
+  }
+
   tmp = getButton();
-  if (tmp == 2) {              // key pressed, switch to the next bank
+  if (tmp == 6) {
+    if (saveEeprom == 0) {
+      saveEeprom = 1;
+      writeEEPROM();
+      setDisplay(18);            // signal put the bank on display
+      delay(500);
+      setDisplay(19);
+      delay(500);
+      setDisplay(romIdx & 0x0F);
+    }
+  } else if (tmp == 3) {        // key pressed, switch to the next bank
     romIdx++;
     romIdx &= 0x0F;
-  } else if (tmp == 3) {       // key pressed for a long time, reset bank to 0
-    romIdx = 0;
-  } else if (tmp == 4) {       // key pressed very long, set current bank as default
-    EEPROM.update(ADDR, romIdx);
-    setDisplay(18);            // signal put the bank on display
-    delay(500);
-    setDisplay(19);
-    delay(500);
+    saveEeprom = 0;
+    //writeEEPROM();
+  } else if (tmp == 4) {        // key pressed for a long time, set current bank as default
+    //romIdx = 0;
+  } else if (tmp == 5) {        // key pressed very long, reset bank to 0
+    //romIdx = 0;
   }
-  delay(10);
+  //delay(10);
 }
